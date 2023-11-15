5179e816a16f641b17f6a40f59a89abbd9080617
Vincent <vincent.hills@gmail.com>
2023-11-15 21:40:40 +0000

    a

--- src/libgca/pid.cpp
+++ src/libgca/pid.cpp
@@ -1,1 +1,17 @@
 #include "libgca/pid.hpp"
+
+
+void PID::reset_variables() {
+  output = 0;
+  target = 0;
+  error = 0;
+  prev_error = 0;
+  integral = 0;
+  time = 0;
+  prev_time = 0;
+}
+
+PID::PID() {
+  reset_variables();
+  set_constants(0, 0, 0, 0);
+}
